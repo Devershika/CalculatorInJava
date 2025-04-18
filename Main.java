556d90d793ae019532561d5796feb87a524374d4
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-04-18 22:31:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,27 @@
+// Name: DEVERSHIKA MOHANE
+// PRN: 23070126032
+// Batch: AIML A2
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        int choice;
+        do {
+            System.out.println("\n---- CALCULATOR MENU ----");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Square");
+            System.out.println("6. Cube");
+            System.out.println("7. Square Root");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+            
+            choice = sc.nextInt();
+
 
