798a821d9e204d5ca4ed0106d5050cbed3c1d0f4
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-04-18 22:37:56 +0530

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -8,4 +8,23 @@ public class Operations {
         return a - b;
     }
 
+    public double multiply(double a, double b) {
+        return a * b;
+    }
 
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double square(double a) {
+        return a * a;
+    }
+
+    public double cube(double a) {
+        return a * a * a;
+    }
+
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+}
